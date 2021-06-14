874c5a07677018916bb1c537ea8f439be0a49500
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2021-06-14 19:18:19 +0530

    sort by new, hot, top flag for reddit

--- cmd/reddit.go
+++ cmd/reddit.go
@@ -5,15 +5,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var sort string
+
 var redditCmd = &cobra.Command{
 	Use:   "reddit",
 	Short: "Set wallpaper from Reddit",
 	Long:  `Selects a random wallpaper from new wallpapers uploaded to several different subreddits`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return reddit.DownloadFromReddit()
+		return reddit.DownloadFromReddit(sort)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(redditCmd)
+	// rootCmd.PersistentFlags().StringVarP(&Resolution, "resolution", "r", "", "Resolution of wallpaper (defaults to 1920x1080)")
+	redditCmd.Flags().StringVarP(&sort, "sort", "s", "new", `Choose from "new", "hot", and "top" sections`)
 }
